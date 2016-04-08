f696a91aad97101ef06837f7398521f01d26a941
Christian Pace <Christian Pace>
2016-04-08 18:27:38 +0200

    project init

---
+++ app.js
@@ -0,0 +1,25 @@
+var hapi = require("hapi");
+var io = require("socket.io");
+
+var server = new hapi.Server();
+
+//set connection details
+var connectionConfig = {
+  port: 8088  
+};
+
+server.connection(connectionConfig);
+
+
+
+
+
+//Start Server
+server.start(function(err){
+   if(err)
+   {
+       throw err;
+   } 
+   
+   console.log("server started and running on %s",server.info.uri);
+});
