26b17942b92c6127f4ec164302ff10f7625ec758
Christian Pace <Christian Pace>
2016-04-16 16:44:25 +0200

    cdpace added test routes

--- modules/routeHandler.js
+++ modules/routeHandler.js
@@ -1,41 +1,21 @@
-var routeHandler = function (args) {
-    ValidateArgs();
-
-    var app = args.app;
-    var fs = args.fs;
+var routeHandler = function (app, fs) {
 
     this.initRoutes = function () {
         //Set Routes for the application
-        app.get("/",function(req,res){
-           res.render("home"); 
+        app.get("/", function (req, res) {
+            res.render("home");
+        });
+        
+        app.get("/test",function(req,res){
+           res.render("test"); 
         });
     };
 
-    //private functions
-    function SendPage(response, data) {
-        CreatePageHeaders(response);
-        response.write(data);
-        response.end();
-    }
-
-    function CreatePageHeaders(response) {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-    }
-
-    function ValidateArgs() {
-        if (args.app == null) {
-            throw "ERROR: routeHandler express module required";
-        }
-
-        if (args.fs == null) {
-            throw "ERROR: routeHandler fs module required";
-        }
-    }
 };
 
 //Export the module
-module.exports = function (fs) {
-    var handler = new routeHandler(fs);
+module.exports = function (app, fs) {
+    var handler = new routeHandler(app, fs);
 
     return handler;
 };
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var fs = require("fs");
-var rh = require("./modules/routeHandler.js")({ app: app, fs: fs });
-var ntepace = require("ntepace")(fs,app,{
-    template : "./pages/layout/_layout.html",
+var rh = require("./modules/routeHandler.js")(app, fs);
+var ntepace = require("ntepace")(fs, app, {
+    template: "./pages/layout/_layout.html",
     viewDir: "./pages/views/"
 });
 
@@ -17,6 +17,6 @@ var connectionConfig = {
 rh.initRoutes();
 
 //Start server
-http.listen(connectionConfig.port, function() {
+http.listen(connectionConfig.port, function () {
     console.log("Server started on: http://localhost:%s", connectionConfig.port);
 });
