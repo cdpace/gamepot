81c585f48a84e74cf4d68b358d584362aac78451
Christian Pace <Christian Pace>
2016-04-21 21:58:05 +0200

    cdpace: changes

--- server.js
+++ server.js
@@ -4,11 +4,50 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var fs = require("fs");
 //var rh = require("./modules/routeHandler.js")(app, fs);
+
+var controllers = {};
+
+//Home Controller
+controllers.HOME = {
+    //Actions
+    GET: {
+        INDEX: function (args) {
+            console.log("accessed get home/index action");
+            return {
+                viewName: "home",
+                args: {}
+            };
+        },
+        TEST: function (args) {
+            console.log("accessing test action");
+            return {
+                viewName: "test",
+                args: {}
+            };
+        }
+    },
+    POST: {
+        INDEX: function (args) {
+            console.log("accessed post home/index action");
+        }
+    }
+};
+
+
 var ntepace = require("ntepace")(fs, app, {
     template: "./pages/layout/_layout.html",
     viewDir: "./pages/views/",
-    handleRoutes: true
+    handleRoutes: true,
+    setRouteHandlerManually: true,
+    controllers: controllers
 });
+
+app.use(function(req,res,next){
+   console.log("custom middleware ");
+   next(); 
+});
+
+app.use(ntepace.routeHandler);
 
 //set configurations
 var connectionConfig = {
