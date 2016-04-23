251fc29e5e080f5eb734dc364b4f988285ce49ac
Christian Pace <Christian Pace>
2016-04-23 19:02:04 +0200

    cdpace: changes

--- server.js
+++ server.js
@@ -1,59 +1,26 @@
 //Modules
+//Vendor Modules
 var bodyParser = require("body-parser");
-var app = require("express")();
+var express = require("express");
+var app = express();
 var http = require("http").Server(app);
-var io = require("socket.io")(http);
 var fs = require("fs");
-
-var controllers = {};
-
-//Home Controller
-controllers.HOME = {
-    //Actions
-    GET: {
-        INDEX: function (args) {
-            console.log("accessed get home/index action");
-            return {
-                viewName: "home",
-                args: {}
-            };
-        },
-        TEST: function (args) {
-            console.log("accessing test action");
-            return {
-                viewName: "test",
-                args: {}
-            };
-        }
-    },
-    POST: {
-        INDEX: function (args) {
-            console.log("accessed post home/index action");
-        },
-        TEST: function(args){
-            console.log("accessed from post action");
-            return {
-                viewName: "test",
-                args: {
-                    name : args[0].name
-                }
-            };
-        }
-    }
-};
-
-
+//Custom Modules
+var controllers = require("./modules/controllers");
 var ntepace = require("ntepace")(fs, app, {
     template: "./pages/layout/_layout.html",
     viewDir: "./pages/views/",
     controllers: controllers
 });
 
+//Register Middleware
 app.use(function (req, res, next) {
     console.log("custom middleware ");
     next();
 });
-
+app.use(express.static("./pages/static/images/"));
+app.use(express.static("./pages/static/css/"));
+app.use(express.static("./pages/static/js/"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.urlencoded({ extended: true }));
